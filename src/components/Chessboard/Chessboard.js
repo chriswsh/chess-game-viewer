e9b975490609c7aa46776fa7d872533e9cdca352
Chris Wong Sick Hong <chriswongsickhong@gmail.com>
2017-07-01 14:18:30 -0500

    Added Chessboard, Square Components
    * Added Chessboard and Square components
    * Added basic styling for Chessboard and Square components

---
+++ src/components/Chessboard/Chessboard.js
@@ -0,0 +1,48 @@
+import './Chessboard.css';
+
+import React, { Component } from 'react';
+import { Square } from '../Square/Square';
+import { Piece } from '../Piece/Piece';
+
+import { BEMcombine } from '../../utils/settings'
+
+const componentTree = [`Chessboard`];
+
+export default class Chessboard extends Component {
+
+    renderSquare(i) {
+        return (
+            <Square
+                color = {Chessboard.squareColor(i)}
+                key = {i}
+                id = {Chessboard.ANname(i)}
+            />
+        );
+    }
+
+    render() {
+        const squares = [];
+        // squares[0] is a8
+        for (let i = 0; i < 64; i++) {
+            squares.push(this.renderSquare(i));
+        }
+        return (
+            <div className={BEMcombine(componentTree)([])}>
+                {squares}
+            </div>
+        );
+    }
+
+    // I convert a square key (0-63) to the square's name in algebraic notation
+    // 0 = top right corner, or a8
+    static ANname(i) {
+        return String.fromCharCode(97 + (i % 8)) + (8 - Math.floor(i / 8));
+    }
+
+    static squareColor(i) {
+        let color = i % 2;
+        color = (Math.floor(i / 8) % 2) ? Math.abs(color - 1) : color;
+
+        return color ? `dark` : `light`;
+    }
+}
---
+++ src/components/Square/Square.js
@@ -0,0 +1,22 @@
+import './Square.css';
+
+import { BEMcombine } from '../../utils/settings'
+
+import React from 'react';
+import { Piece } from '../Piece/Piece';
+
+const componentTree = [`Square`];
+
+// Let's use stateless functional components - we're jumping into functional programming, after all
+export function Square(props) {
+    // construct class name from props
+    const squareBase = BEMcombine(componentTree);
+    const squareClass = props.color ? [squareBase([]), squareBase([props.color])].join(` `) : squareBase([]);
+
+    return (
+        <div className={squareClass}>
+            {props.id}
+            <Piece />
+        </div>
+    );
+}
