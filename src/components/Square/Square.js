40359a96651ab0463677b432f1d814506e21e494
Chris Wong Sick Hong <chriswongsickhong@gmail.com>
2017-07-04 12:58:36 -0500

    Reverted `Square` to stateless functional component

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -5,18 +5,12 @@ import { withBEM } from '../../utils/BEM';
 import Piece from '../Piece/Piece';
 import Chess from '../../utils/Chess';
 
-class Square extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className={ this.props.BEMclass } id={ this.props.id }>
-                <Piece piece={ this.props.piece } BEMmodifiers={ [Chess.getPieceColor(this.props.piece)] } />
-            </div>
-        );
-    }
+function Square(props) {
+    return (
+        <div className={ props.BEMclass } id={ props.id }>
+            <Piece piece={ props.piece } BEMmodifiers={ [Chess.getPieceColor(props.piece)] } />
+        </div>
+    );
 }
 
 // Export the BEM-Wrapped Component with the samename as the .js file
