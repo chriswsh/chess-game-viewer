c0d83ba8fc51ce102c87624516bb2dd832e2ac97
Chris Wong Sick Hong <chriswongsickhong@gmail.com>
2017-07-09 13:03:48 -0500

    Added Provider component for react-redux
    * Added provider component for react-redux
    * Used noop for placeholder reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(() => {});
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
