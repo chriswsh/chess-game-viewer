f138ac26987571e62e7320ab71c3472b25593516
Chris Wong Sick Hong <chriswongsickhong@gmail.com>
2017-07-11 21:38:32 -0500

    Added store to smoke tests

--- src/components/ChessViewer/ChessViewer.test.js
+++ src/components/ChessViewer/ChessViewer.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import ChessViewer from './ChessViewer';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import chessViewerReducer from '../../reducers/chessViewerReducer';
+
+let store = createStore(chessViewerReducer);
+
 it('renders without crashing', () => {
-    const rendered = mount(<ChessViewer />);
+    const rendered = mount(<Provider store={ store }><ChessViewer/></Provider>);
 });
