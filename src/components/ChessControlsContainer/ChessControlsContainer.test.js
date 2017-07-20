7ec2a11028aed3a5f670e1a6f00ae2a10d33cc59
Chris Wong Sick Hong <chriswongsickhong@gmail.com>
2017-07-19 21:38:27 -0500

    Fixed bugs in test suite
    * NaN !== Nan, so need to use isNaN instead
    * Need to test for both Infinity and -Infinity

--- src/components/ChessControlsContainer/ChessControlsContainer.test.js
+++ src/components/ChessControlsContainer/ChessControlsContainer.test.js
@@ -15,12 +15,12 @@ it(`renders without crashing`, () => {
 it(`should pass currentMove and lastMove props to the wrapped component, also called ChessControlsContainer`, () => {
     const rendered = mount(<Provider store={ store }><ChessControlsContainer/></Provider>);
     expect(rendered.find(`ChessControlsContainer`).props().currentMove).toEqual(expect.any(Number));
-    expect(rendered.find(`ChessControlsContainer`).props().currentMove).not.toEqual(NaN);
-    expect(rendered.find(`ChessControlsContainer`).props().currentMove).not.toEqual(Infinity);
+    expect(isNaN(rendered.find(`ChessControlsContainer`).props().currentMove)).toEqual(false);
+    expect(Math.abs(rendered.find(`ChessControlsContainer`).props().currentMove)).not.toEqual(Infinity);
     
     expect(rendered.find(`ChessControlsContainer`).props().lastMove).toEqual(expect.any(Number));
-    expect(rendered.find(`ChessControlsContainer`).props().lastMove).not.toEqual(NaN);
-    expect(rendered.find(`ChessControlsContainer`).props().currentMove).not.toEqual(Infinity);
+    expect(isNaN(rendered.find(`ChessControlsContainer`).props().lastMove)).toEqual(false);
+    expect(Math.abs(rendered.find(`ChessControlsContainer`).props().currentMove)).not.toEqual(Infinity);
 });
 
 it(`should pass currentMove and lastMove props of 0 with the default store values (no PGN loaded)`, () => {
