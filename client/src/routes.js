930e1289e3914a8b444dce1cf075edd09ed5b5b8
Chris Wong Sick Hong <chriswongsickhong@gmail.com>
2017-08-09 22:15:17 -0500

    Switched to `BrowserRouter`

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Switch, Router, Route } from 'react-router';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App';
 import NotFound from './components/NotFound/NotFound';
 
 const Routes = (props) => (
-    <Router {...props}>
+    <BrowserRouter {...props}>
         <Switch>
             <Route exact path="/" component={App} />
             <Route component={NotFound} />
         </Switch>
-    </Router>
+    </BrowserRouter>
 );
 
 export default Routes;
